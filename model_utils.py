33a7b8b031429d93f2e3111beb987c6731ac3dc7
LucasHartman <lucas.hartman@kognitive.ca>
2023-07-01 15:03:21 -0400

    Force use of cpu

--- model_utils.py
+++ model_utils.py
@@ -3,8 +3,9 @@ from train import BigramLanguageModel, encode, decode, device
 
 # load the saved model
 def load_model(model_load_path):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     loaded_model = BigramLanguageModel()
-    loaded_model.load_state_dict(torch.load(model_load_path))
+    loaded_model.load_state_dict(torch.load('bigram_language_model.pt', map_location=device))
     loaded_model = loaded_model.to(device)
     return loaded_model
 
